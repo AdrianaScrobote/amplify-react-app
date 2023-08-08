b9eb0eb852847ed0777c5c031a8eb7c7ac60a554
Adriana Scrobote <adriana_100000@hotmail.com>
2023-08-07 22:42:40 -0300

    Adicionando tela de listagem de músicas com numero de likes - DynamoDB e React com Amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "songAPI": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyreactappe3243a5f": {
       "AppClientID": "string",
--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
 import "./App.css";
-import { Amplify } from "aws-amplify";
-import { withAuthenticator, Authenticator } from "@aws-amplify/ui-react";
+import { Amplify, API, graphqlOperation } from "aws-amplify";
+import { withAuthenticator, Authenticator, Icon } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
+import { listSongs } from "./graphql/queries";
+import { updateSong } from "./graphql/mutations";
+
+import { Paper, IconButton } from "@material-ui/core";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import FavoriteIcon from "@material-ui/icons/Favorite";
 
 import awsconfig from "./aws-exports";
-import React from "react";
+import React, { useEffect, useState } from "react";
 Amplify.configure(awsconfig);
 
 function App() {
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    fetchSongs();
+  }, []);
+
+  const fetchSongs = async () => {
+    try {
+      const songData = await API.graphql(graphqlOperation(listSongs));
+      console.log("songData", songData);
+      const songList = songData.data.listSongs.items;
+      console.log("songList", songList);
+      setSongs(songList);
+    } catch (error) {
+      console.log("error on fetching songs", error);
+    }
+  };
+
+  const addLike = async (idx) => {
+    try {
+      const song = songs[idx];
+      song.like = song.like + 1;
+      delete song.createdAt;
+      delete song.updatedAt;
+
+      const songData = await API.graphql(
+        graphqlOperation(updateSong, { input: song })
+      );
+      const songList = [...songs];
+      songList[idx] = songData.data.updateSong;
+      setSongs(songList);
+    } catch (error) {
+      console.log("error on adding Like to song", error);
+    }
+  };
+
   return (
     <Authenticator>
       {({ signOut, user }) => (
@@ -17,6 +59,33 @@ function App() {
               <button onClick={signOut}>Sign out</button>
               <h2>My App Content</h2>
             </header>
+            <div className="songList">
+              {songs.map((song, idx) => {
+                return (
+                  <Paper variant="outlined" elevation={2} key={`song${idx}`}>
+                    <div className="songCard">
+                      <IconButton aria-label="play">
+                        <PlayArrowIcon />
+                      </IconButton>
+                      <div>
+                        <div className="SongTitle">{song.title}</div>
+                        <div className="SongOwner">{song.owner}</div>
+                      </div>
+                      <div>
+                        <IconButton
+                          aria-label="like"
+                          onClick={() => addLike(idx)}
+                        >
+                          <FavoriteIcon />
+                        </IconButton>
+                        {song.like}
+                      </div>
+                      <div className="SongDescription">{song.description}</div>
+                    </div>
+                  </Paper>
+                );
+              })}
+            </div>
           </div>
         </main>
       )}
